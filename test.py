b17c935f5d5a1c7a22ea4a47c104fb52bc244763
Ben Pringle <ben.pringle@gmail.com>
2014-04-13 17:26:30 -0400

    Add test

---
+++ test.py
@@ -0,0 +1,12 @@
+import asyncio
+import greenio
+import asyncreq
+
+@asyncio.coroutine
+def test():
+    response = yield from asyncreq.request('GET', 'http://www.google.com')
+    print(response.text)
+
+asyncio.set_event_loop_policy(greenio.GreenEventLoopPolicy())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test())
